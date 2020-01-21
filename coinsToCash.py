69d28dd572a154de3b7288e66e6c3346d7c9a1d8
joseph walker <josephwalker504@gmail.com>
2020-01-21 14:39:34 -0600

    completed first part of practice exercise

---
+++ coinsToCash.py
@@ -0,0 +1,25 @@
+import math
+
+# # Create a function called calc_dollars. In the function body, define a dictionary 
+# # and store it in a variable named piggyBank. The dictionary should have the 
+# # following keys defined. quarters,dimes, nickels, and pennies
+# # For each coin type, give yourself as many as you like.
+
+def calc_dollars():
+    piggyBank = {
+        "quarters": 77,
+        "dimes": 107,
+        "nickels": 71,
+        "pennies": 7
+    }
+
+
+# Once you have given yourself a large stash of coins in your piggybank, 
+# look at each key and perform the appropriate math on the integer value 
+# to determine how much money you have in dollars. Store that value in a 
+# variable named dollarAmount and print it.
+
+
+dollarAmount = {
+    
+}
